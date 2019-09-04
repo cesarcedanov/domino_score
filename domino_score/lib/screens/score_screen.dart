d3cacb32057223061e929ac64809096a05ad0404
CesarCedanoV <CesarCedanoV@gmail.com>
2019-09-04 00:50:28 -0400

    Created three new screens [Settings, Score, History] and added them into a TabsNavigation

--- domino_score/lib/screens/score_screen.dart
+++ domino_score/lib/screens/score_screen.dart
@@ -1,49 +1,11 @@
 import 'package:flutter/material.dart';
 
 class ScoreScreen extends StatelessWidget {
+  static const routeName = '/score';
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Domino Score'),
-        actions: <Widget>[
-          FlatButton(
-            child: Text(
-              'NEW GAME',
-              style: TextStyle(color: Colors.white),
-            ),
-            onPressed: () {},
-          )
-        ],
-      ),
-      body: Column(
-        children: <Widget>[
-          Center(
-            child: Text('SCORE BOARD'),
-          ),
-        ],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            title: Text('Settings'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.videogame_asset),
-            title: Text('Score'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.history),
-            title: Text('History'),
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Theme.of(context).primaryColor,
-        child: Icon(Icons.add),
-        onPressed: () {},
-      ),
+    return Center(
+      child: Text('SCORE BOARD'),
     );
   }
 }
---
+++ domino_score/lib/screens/tabs_navigation.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import './settings_screen.dart';
+import './score_screen.dart';
+import './history_screen.dart';
+
+class TabsNavigation extends StatefulWidget {
+  @override
+  _TabsNavigationState createState() => _TabsNavigationState();
+}
+
+class _TabsNavigationState extends State<TabsNavigation> {
+  List<Map<String, Object>> _pages;
+
+  int _selectedTabIndex = 1;
+
+  @override
+  void initState() {
+    _pages = [
+      {
+        "title": "Settings",
+        "screen": SettingsScreen(),
+        "actions": <Widget>[],
+      },
+      {
+        "title": "Score",
+        "screen": ScoreScreen(),
+        "actions": <Widget>[
+          FlatButton(
+            child: Text(
+              'NEW GAME',
+              style: TextStyle(color: Colors.white),
+            ),
+            onPressed: () {},
+          )
+        ],
+      },
+      {
+        "title": "History",
+        "screen": HistoryScreen(),
+        "actions": <Widget>[],
+      },
+    ];
+    super.initState();
+  }
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedTabIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_pages[_selectedTabIndex]["title"]),
+        actions: _pages[_selectedTabIndex]["actions"],
+      ),
+      body: _pages[_selectedTabIndex]["screen"],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        unselectedItemColor: Colors.black,
+        selectedItemColor: Theme.of(context).primaryColor,
+        currentIndex: _selectedTabIndex,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            title: Text('Settings'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.videogame_asset),
+            title: Text('Score'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.history),
+            title: Text('History'),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Theme.of(context).primaryColor,
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
+    );
+  }
+}
